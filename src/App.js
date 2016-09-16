f76d5fedb6a829670fb5b7d1475c557bcccae706
Christoph Weber <kriswep@wetainment.com>
2016-09-16 23:54:08 +0200

    add nav

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router'
 import logo from './logo.svg';
 import './App.css';
 
-import Teams from './container/Teams';
+import NavLink from './presentation/NavLink';
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Link to="/">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Temer</h2>
         </div>
-        <Teams/>
+        </Link>
+        <ul>
+          <li><NavLink to="/teams">Manage Teams</NavLink></li>
+        </ul>
+        
+        {this.props.children}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router'
 
-import App from './App';
 import './index.css';
+
+import App from './App';
+import Teams from './container/Teams';
 
 ReactDOM.render((
   <Router history={hashHistory}>
-    <Route path="/" component={App}/>
+    <Route path="/" component={App}>      
+      <Route path="/teams" component={Teams}/>
+    </Route>
   </Router>
 ), document.getElementById('root'));
